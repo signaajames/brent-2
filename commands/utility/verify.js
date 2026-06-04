bb42a47965ad0d9cdf64b1fc1c48e5aafa27931b
signaajames <penguingnop@gmail.com>
2026-06-04 17:25:44 +0800

    moved the embed and button to be inside of execute

--- commands/utility/verify.js
+++ commands/utility/verify.js
@@ -9,21 +9,6 @@ export const data = new SlashCommandBuilder()
   .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
   .addChannelOption(option => option.setName('channel').setDescription('The channel to send the message in').setRequired(true))
   .addRoleOption(option => option.setName('role').setDescription('The role to give upon verification').setRequired(true))
-
-// Embed:
-const embed = new EmbedBuilder()
-  .setTitle('Verification')
-  .setDescription('Click the button to start the verification process. We do this to prevent people using an alt to evade bans. And to show you\'re a functioning human that isn\'t some 8 year old. This works pretty much exactly like **double counter**.')
-  .setColor("Blue")
-
-// Button:
-const button = new ButtonBuilder()
-  .setCustomId('verify:')
-  .setLabel('Verify')
-  .setStyle(ButtonStyle.Primary)
-
-const row = new ActionRowBuilder().addComponents(button)
-
 /**
  * @param {{ options: { getChannel: (arg0: string) => any; }; send: (arg0: { content: string; ephemeral: boolean; }) => any; }} interaction
  */
@@ -31,6 +16,20 @@ export async function execute(interaction) {
   await interaction.deferReply({ flags: MessageFlags.Ephemeral })
   const channel = interaction.options.getChannel('channel')
   const role = interaction.options.getRole('role')
+
+  // Embed:
+  const embed = new EmbedBuilder()
+    .setTitle('Verification')
+    .setDescription('Click the button to start the verification process. We do this to prevent people using an alt to evade bans. And to show you\'re a functioning human that isn\'t some 8 year old. This works pretty much exactly like **double counter**.')
+    .setColor("Blue")
+
+  // Button:
+  const button = new ButtonBuilder()
+    .setCustomId('verify:' + role.id)
+    .setLabel('Verify')
+    .setStyle(ButtonStyle.Primary)
+
+  const row = new ActionRowBuilder().addComponents(button)
 
   await channel.send({ embeds: [embed], components: [row] })
   console.log(interaction.user.username, "sent the verify message in", channel.name)
